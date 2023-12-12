adc61fb6437d884ab3a41b8122b63d0207c7d23c
喵二 <66155308+miaoermua@users.noreply.github.com>
2023-12-12 18:07:30 +0800

    Update cattools.sh

--- cattools.sh
+++ cattools.sh
@@ -115,12 +115,12 @@ while :; do
     clear
     echo "CatTools"
     echo "---------------------------"  
-    echo "1. Set IPv4 设置默认IP"
-    echo "2. Check Update 检查系统更新"
-    echo "3. network diagnostics 网络诊断" 
-    ehco "4. use repo 使用软件源"
-    ehco "5. sysupgrade 升级系统"
-    echo "0. Exit 退出脚本"
+    echo "- 1. -Set IPv4 设置默认IP"
+    echo "- 2. - Check Update 检查系统更新"
+    echo "- 3. -network diagnostics 网络诊断"
+    ehco "- 4. - use repo 使用软件源"
+    ehco "- 5. -sysupgrade 升级系统"
+    echo "- 0. - Exit 退出脚本"
     read -p "请选择: " choice
 
     case $choice in
