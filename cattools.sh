a7ab313e4bb417de7946bb28f84a6256cabad920
喵二 <66155308+miaoermua@users.noreply.github.com>
2024-07-27 18:46:17 +0800

    Update cattools.sh

--- cattools.sh
+++ cattools.sh
@@ -1324,7 +1324,7 @@ configure_tailscale() {
     subnet="$i1.$i2.$i3.0/$mask"
 
     echo "[INFO] 下载配置..."
-    curl -fsSL https://raw.githubusercontent.com/miaoermua/cattools/main/configure/tailscale.sh -o /tmp/tailscale.sh
+    curl -fsSL https://mirror.ghproxy.com/https://raw.githubusercontent.com/miaoermua/cattools/main/configure/tailscale.sh -o /tmp/tailscale.sh
     if [ $? -ne 0 ]; then
         echo "[ERROR] 下载配置失败..."
         menu
