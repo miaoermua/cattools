eb9e3b3d9456da7783c24d695117d691a1719712
喵二 <66155308+miaoermua@users.noreply.github.com>
2025-07-26 16:51:56 +0800

    update dev mode

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ fi
 
 # installed
 install_cattools() {
-set -
+set -x
     if [ ! -f /usr/bin/cattools ]; then
         echo "cattools not found, installing..."
         if curl --silent --connect-timeout 5 -o /usr/bin/cattools https://raw.miaoer.net/cattools/cattools.sh; then
@@ -28,7 +28,7 @@ set -
     fi
     echo "cattools is installed successfully!"
     echo "please type ‘cattools’ or ‘/usr/bin/cattools’ to run it."
-set +
+set +x
 }
 
 install_cattools
